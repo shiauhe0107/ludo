760cfff5fbb24e100d93c90afd41d11e276522c7
unknown <kevin010720@yahoo.com.tw>
2021-09-07 02:42:46 +0800

    FRONT

---
+++ web-desktop/src/settings.22358.js
@@ -0,0 +1,23 @@
+window._CCSettings = {
+    platform: "web-desktop",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scene/0.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "a8060",
+        main: "088ab"
+    }
+};
